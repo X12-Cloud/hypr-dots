c2ce997c98ebdd04db9e8a19e390a95806ab4720
X12-Cloud <lamadasun888@gmail.com>
2026-02-13 16:02:30 +0200

    polishing

--- qutebrowser/.config/qutebrowser/config.py
+++ qutebrowser/.config/qutebrowser/config.py
@@ -21,6 +21,21 @@ config.set('url.searchengines', {
 config.set('fonts.default_size', '12pt')
 config.set('colors.webpage.darkmode.enabled', True) # Permanent Dark Mode
 config.set('content.blocking.method', 'both')      # Better Ad-blocking
+c.auto_save.session = True
+c.session.lazy_restore = True
+
+c.fonts.default_family = "JetBrainsMono Nerd Font"
+c.fonts.web.family.standard = "" # Leave empty to let the site decide
+c.fonts.web.family.sans_serif = "JetBrainsMono Nerd Font"
+c.fonts.web.family.fixed = "JetBrainsMono Nerd Font"
+c.colors.webpage.preferred_color_scheme = 'dark'
+c.qt.args = ['enable-gpu-rasterization', 'ignore-gpu-blocklist']
+
+c.colors.tabs.even.bg = "#1C1C1E"
+c.colors.tabs.odd.bg = "#1C1C1E"
+c.colors.tabs.selected.even.bg = "#2C2C2E"
+c.colors.tabs.selected.odd.bg = "#2C2C2E"
+c.colors.statusbar.normal.bg = "#1C1C1E"
 
 # Binds
 config.bind('<Ctrl-t>', 'cmd-set-text -s :open -t', mode='normal')

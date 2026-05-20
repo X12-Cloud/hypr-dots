7afde43d789bb93ad4eef8b5e46b37814ab4faa5
X12-Cloud <lamadasun888@gmail.com>
2026-05-20 18:49:01 +0300

    thingy

--- hypr/.config/hypr/hyprland/windows.lua
+++ hypr/.config/hypr/hyprland/windows.lua
@@ -2,3 +2,28 @@
 -- Source: /home/x12/dotfiles/hypr/.config/hypr/hyprland/windows.conf
 -- Some values might need MANUAL check. PLEASE DO BACKUP BEFORE TESTING, PLEASEEEE.
 
+-- Foot
+hl.window_rule({
+	match = { class = "^(foot)$" },
+	opacity = "0.85 override 0.80 override",
+	-- blur = true,
+})
+
+-- Nautilus
+hl.window_rule({
+	match = { class = "^(nautilus)$" },
+	opacity = "0.85 override 0.80 override",
+	-- blur = true,
+})
+
+-- Spotify
+hl.window_rule({
+	match = { class = "^(spotify)$" },
+	opacity = "0.80 override 0.75 override",
+})
+
+-- Discord
+hl.window_rule({
+	match = { class = "^(discord)$" },
+	opacity = "0.80 override 0.75 override",
+})
